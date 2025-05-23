e787959df5833627efc30005f7a79769baf79713
alyson02 <alyson.subrim2002@gmail.com>
2025-05-23 17:30:25 -0300

    Add sun emoji to celebration message

--- Solaire.Core/Solarie.cs
+++ Solaire.Core/Solarie.cs
@@ -4,5 +4,5 @@ public static class Solarie
 {
     public static string Celebrate() => $"Thank you to use that package im happy to make this and i wold to say praise the sun!! {ImProveCelebration()}";
 
-    private static string ImProveCelebration() => "\\[T]/";
+    private static string ImProveCelebration() => "\\[T]/ ☀️";
 }
