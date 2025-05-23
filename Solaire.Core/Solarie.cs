6a6dcf13158b985f0f5d84ac3b906275ce916134
alyson02 <alyson.subrim2002@gmail.com>
2025-05-22 22:27:06 -0300

    Add Solarie class and XUnit global using
    Introduce the `Solarie` class with a `Celebrate` method and a private helper `ImProveCelebration` to generate a celebratory message. Also, set up a global using directive for XUnit in test projects to simplify test development.

---
+++ Solaire.Core/Solarie.cs
@@ -0,0 +1,8 @@
+﻿namespace Solaire.Core;
+
+public static class Solarie
+{
+    public static string Celebrate() => $"Thank you to use that package im happy to make this and i wold to say praise the sun!! {ImProveCelebration()}";
+
+    private static string ImProveCelebration() => "\\[T]/";
+}
